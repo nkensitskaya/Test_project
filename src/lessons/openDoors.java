4f17dbd4619bc1211a823e5a23ea13fc2daec257
nkatiuzhenko <nkatiuzhenko@playtika.com>
2018-03-03 18:30:32 +0200

    100 doors task

---
+++ src/lessons/openDoors.java
@@ -0,0 +1,43 @@
+package lessons;
+
+public class openDoors {
+
+	public static void main(String[] args) {
+		int[] doors = new int[100];
+		initDoorsStatus(doors);
+		printDoorsStatus(doors);
+
+		for (int i = 0; i < 100; i++){
+			for (int j = i; j<100; j++) {
+				if ((j+1)%(i+1) == 0 ){
+				doors[j] = updateDoorStatus(doors[j]);
+				}
+			}
+			printDoorsStatus(doors);
+		}
+
+		printDoorsStatus(doors);
+
+	}
+
+	private static int updateDoorStatus(int status){
+		return (status+1) % 2;
+	}
+
+	private static void printDoorsStatus(int[] doors) {
+		for (int num : doors) {
+			if (doors[num]==0) {
+				System.out.print("[X]");
+			} else {
+				System.out.print("[O]");
+			}
+		}
+		System.out.println();
+	}
+
+	private static void initDoorsStatus(int[] mass) {
+		for (int i=0; i<mass.length; i++){
+			mass[i] = 0;
+		}
+	}
+}
