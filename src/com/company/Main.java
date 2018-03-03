b8cfd76b181bbba6ccae6a1eb86ae187c549c989
nkatiuzhenko <nkatiuzhenko@playtika.com>
2018-03-03 08:13:11 +0200

    Multiplication table print to console

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+        for (int i=1; i<=10; i++){
+        	for (int j=1; j<=10; j++){
+        		printFormated(i*j);
+			}
+			System.out.println();
+		}
+
+	}
+
+	private static void printFormated(int k) {
+		if (k<10){
+			System.out.print(" " + k + " ");}
+			else {	System.out.print(k + " ");}
+	}
+
 }
